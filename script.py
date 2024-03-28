d3f6069df7965ac92f94b03b59152dba75c495e5
Gabryell Leal Rocha <142511788+lealgabryell@users.noreply.github.com>
2024-03-28 14:09:15 -0300

    Script in python I used to access Yfinance

---
+++ script.py
@@ -0,0 +1,11 @@
+import yfinance as yf
+
+def carregar_dados(ticker):
+    df = yf.Ticker(ticker).history("2y")
+    df.reset_index(inplace=True)
+    df['ticker'] = ticker.split(".")[0]
+    return df
+
+petrobras = carregar_dados("PETR4.SA")
+bb = carregar_dados("BBAS3.SA")
+vale = carregar_dados("VALE3.SA")
